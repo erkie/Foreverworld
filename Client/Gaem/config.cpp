3c383e0228cfe16883dd428d7c7780aa40cb957a
Erik Andersson <erik.rothoff@gmail.com>
2010-12-18 18:08:13 +0100

    Should now connect to master server

--- Client/Gaem/config.cpp
+++ Client/Gaem/config.cpp
@@ -36,7 +36,7 @@ namespace Gaem
 			_values["window_width"] = "1000";
 			_values["window_height"] = "700";
 			
-			_values["server_host"] = "localhost";
+			_values["server_host"] = "glonk.se";
 			_values["server_port"] = "6010";
 		}
 		else
